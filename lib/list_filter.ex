370110d662751d7cba4d8e88ade7bdc8d8ccd815
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2021-07-09 15:59:11 -0300

    challenge complete

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,11 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
+  require Integer
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(list) do
+    list
+    |> Enum.filter(fn list -> String.match?(list, ~r/[0-9]+/) end)
+    |> Enum.map(fn element -> String.to_integer(element) end)
+    |> Enum.filter(fn x -> Integer.is_odd(x) == true end)
+    |> Enum.count()
   end
 end
